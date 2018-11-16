84e212140024fd833d1772eb2b95ef1fe7c8cae7
SuttipatNew <suttipat.new@gmail.com>
2018-11-15 20:10:54 -0800

    fixed bug

--- elastic_search_index.py
+++ elastic_search_index.py
@@ -11,17 +11,25 @@ start_time = time.time()
 ES_HOST = 'http://localhost:9200'
 INDEX ='moovle'; DOCTYPE = 'webpage'
 es = Elasticsearch(ES_HOST)
-f = open('pages.json')
+f = codecs.open('pages.json', 'r','utf-8')
 nb_doc = 0
+line_count = 0
 for doc in f:
-    doc = json.loads(doc)
-    del doc['_id']
-    del doc['html']
-    index_doc(doc)
-    nb_doc += 1
-    if nb_doc % 100 == 0:
-        print('.', end='')
-        sys.stdout.flush()
+    line_count += 1
+    if doc.replace(' ', '').replace('\n', '') == '':
+        continue
+    try:
+        doc = json.loads(doc)
+        del doc['_id']
+        del doc['html']
+        index_doc(doc)
+        nb_doc += 1
+        if nb_doc % 100 == 0:
+            print('.', end='')
+            sys.stdout.flush()
+    except Exception:
+        with codecs.open('log.txt', 'w', 'utf-8') as f:
+            f.write(f'error: line {line_count}\n')
 f.close()
 print(f'\n{nb_doc} documents indexed...')
 end_time = time.time()
